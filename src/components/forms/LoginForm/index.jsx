f9154c55e3126a32378c23dbf95ce685979fe2d1
Bobooshodi <feyi.tobi@gmail.com>
2022-03-13 02:04:19 +0800

    Login page update

--- src/components/forms/LoginForm/index.jsx
+++ src/components/forms/LoginForm/index.jsx
@@ -13,12 +13,6 @@ function LoginForm() {
   return (
     <div className="form">
       <div className="formContainer">
-        <img
-          width={130}
-          src="https://imgs.search.brave.com/JeoPWN1ioE26YmEwMsGByYklCD4VPPGdI9pOVPqICNg/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cG5na2V5LmNvbS9w/bmcvZnVsbC8yMDgt/MjA4OTEzNF9yY2Nn/LWxvZ28tcmVkZWVt/ZWQtY2hyaXN0aWFu/LWNodXJjaC1sb2dv/LnBuZw"
-          alt=""
-        />
-        <h2 className="loginHeader">User Login</h2>
         <Form
           autoComplete="off"
           onFinish={onFinish}
@@ -68,9 +62,6 @@ function LoginForm() {
           </Form.Item>
         </Form>
       </div>
-      <p className="rccgMotto">
-        “Jesus Christ the same yesterday, and today, and forever” - Hebrews 13:8
-      </p>
     </div>
   );
 }
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -5,7 +5,16 @@ import { LoginForm } from "../../components";
 function Login() {
   return (
     <div className="loginContainer">
+        <img
+          width={130}
+          src="https://imgs.search.brave.com/JeoPWN1ioE26YmEwMsGByYklCD4VPPGdI9pOVPqICNg/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cG5na2V5LmNvbS9w/bmcvZnVsbC8yMDgt/MjA4OTEzNF9yY2Nn/LWxvZ28tcmVkZWVt/ZWQtY2hyaXN0aWFu/LWNodXJjaC1sb2dv/LnBuZw"
+          alt=""
+        />
+        <h2 className="loginHeader">User Login</h2>
       <LoginForm />
+      <p className="rccgMotto">
+        “Jesus Christ the same yesterday, and today, and forever” - Hebrews 13:8
+      </p>
     </div>
   );
 }
